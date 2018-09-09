02ecff18f8d1937949873a075e1b71378965e376
Ivan Dovgal <limetrack@Ivans-MacBook-Pro.local>
2018-09-09 14:42:02 +0300

    MaterialUI added

--- src/containers/CoreLayout/CoreLayout.js
+++ src/containers/CoreLayout/CoreLayout.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import logo from './logo.svg';
 import './CoreLayout.css';
+
+const theme = createMuiTheme();
 
 class CoreLayout extends Component {
   render() {
     return (
       <Router>
-        <div className="CoreLayout">
-          <header className="CoreLayout-header">
-            <img src={logo} className="CoreLayout-logo" alt="logo" />
-            <h1 className="CoreLayout-title">Welcome to Dovhal.com</h1>
-          </header>
-          <p className="CoreLayout-intro">
-            To get started, edit <code>src/CoreLayout.js</code> and save to reload.
-          </p>
-        </div>
+        <MuiThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="CoreLayout">
+            <header className="CoreLayout-header">
+              <img src={logo} className="CoreLayout-logo" alt="logo" />
+              <h1 className="CoreLayout-title">Welcome to Dovhal.com</h1>
+            </header>
+          </div>
+        </MuiThemeProvider>
       </Router>
     );
   }
