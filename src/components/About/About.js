37fc0d7b78d63ee556af1f689e7c1e851531f089
Ivan Dovhal <limetrack@gmail.com>
2018-10-15 12:20:30 +0300

    About section updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Paper, Typography } from '@material-ui/core';
+import { MobileFriendly, LaptopMac, HeadsetMic } from '@material-ui/icons';
 import { SectionLayout } from 'containers';
 import styles from './About.style.js';
 
@@ -12,11 +13,60 @@ class About extends Component {
     return (
       <section className={classes.about}>
         <SectionLayout>
-          <Grid container spacing={32} justify="center" alignItems="center" direction="column" className={classes.main}>
-            <Grid item xs={6}>
-              <Typography variant="h2" color="primary" gutterBottom>
-                ABOUT
-              </Typography>
+          <Grid container spacing={32} justify="center" className={classes.main}>
+            <Grid item xs={12}>
+              <Paper className={classes.paper} elevation={1}>
+                <Grid container spacing={32} justify="center">
+                  <Grid item xs={12}>
+                    <Typography variant="subtitle2" align="center" color="primary" className={classes.title}>
+                      About company
+                    </Typography>
+                    <Typography variant="h4" align="center">
+                      We Provided Services All Over The World
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} md={8}>
+                    <Typography variant="subtitle2" color="textSecondary" align="center" className={classes.description}>
+                      Dovhal Ltd. designs and builds beautiful world-class iOS, Android, and Web apps for startups and big brands. 
+                      We bring together the best customer experience talent to hear what users have to say and turn thoughtful 
+                      solutions into apps that make an impact. One user at a time, we’re improving lives.
+                    </Typography>
+                  </Grid> 
+                  <Grid item xs={12}>
+                    <Grid container spacing={32} justify="center">
+                      <Grid item xs={12} md={4}>
+                        <MobileFriendly fontSize="large" color="primary" className={classes.icon} />
+                        <Typography variant="h6" align="center">
+                          App Development
+                        </Typography>
+                        <Typography variant="subtitle2" color="textSecondary" align="center" className={classes.description}>
+                          We use our expertise in a variety of technologies to craft code that opens the door for ongoing iterations 
+                          with our clients’ users. Our teams ensure a build with the foundation for success.
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={12} md={4}>
+                        <LaptopMac fontSize="large" color="primary" className={classes.icon} />
+                        <Typography variant="h6" align="center">
+                          Web Development
+                        </Typography>
+                        <Typography variant="subtitle2" color="textSecondary" align="center" className={classes.description}>
+                          Our web development process is geared toward an intuitive experience for users. We create products that are 
+                          both aesthetically pleasing and user friendly.
+                        </Typography>
+                      </Grid> 
+                      <Grid item xs={12} md={4}>
+                        <HeadsetMic fontSize="large" color="primary" className={classes.icon} />
+                        <Typography variant="h6" align="center">
+                          Support
+                        </Typography>
+                        <Typography variant="subtitle2" color="textSecondary" align="center" className={classes.description}>
+                          A launch date doesn’t mean “goodbye.” We continue collaborating for exponential success, keeping you and your products online.
+                        </Typography>
+                      </Grid> 
+                    </Grid>
+                  </Grid> 
+                </Grid>
+              </Paper>
             </Grid>
           </Grid>
         </SectionLayout>
--- src/components/About/About.style.js
+++ src/components/About/About.style.js
@@ -1,7 +1,22 @@
 export default (theme) => ({
   about: {
+    top: '-50px',
     minHeight: 600,
     position: 'relative',
-    backgroundColor: theme.palette.grey['100'],
+    // backgroundColor: theme.palette.grey['100'],
   },
+  paper: {
+    paddingTop: `${theme.spacing.unit * 6}px`,
+    paddingBottom: `${theme.spacing.unit * 6}px`,
+  },
+  title: {
+    textTransform: 'uppercase',
+  },
+  description: {
+    margin: `0 ${theme.spacing.unit * 5}px`,
+  },
+  icon: {
+    width: '100%',
+    margin: `${theme.spacing.unit * 2}px auto`,
+  }
 })
