9fabd44da7024d229f9f9e5c03657f9f5de25000
Ivan Dovhal <limetrack@gmail.com>
2018-10-15 18:00:38 +0300

    Phrase section updated

--- src/components/Phrase/Phrase.style.js
+++ src/components/Phrase/Phrase.style.js
@@ -1,6 +1,19 @@
+import planet from './img/planet.png';
+
 export default (theme) => ({
   phrase: {
-    minHeight: 600,
-    backgroundColor: theme.palette.grey['100'],
-  }
+    minHeight: `${theme.spacing.unit * 80}px`,
+    // backgroundColor: theme.palette.grey['100'],
+    backgroundImage: `url(${planet})`,
+    backgroundSize: 'auto',
+    backgroundPosition: 'right',
+    backgroundRepeat: 'no-repeat',
+  },
+  photo: {
+    maxWidth: '100%',
+    maxHeight: `${theme.spacing.unit * 70}px`,
+  },
+  me: {
+    marginLeft: '2px',
+  },
 })
