e4b64d2a5aab4850ba13d143142ca0fbda4a01a7
Ivan Dovhal <limetrack@gmail.com>
2018-10-15 22:36:25 +0300

    Services section updated

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Paper, Typography } from '@material-ui/core';
+import { FingerprintOutlined, HighQualityOutlined, PeopleOutlined, VerifiedUserOutlined } from '@material-ui/icons';
 import { SectionLayout } from 'containers';
 import styles from './Services.style.js';
 
@@ -12,12 +13,69 @@ class Services extends Component {
     return (
       <section className={classes.services}>
         <SectionLayout>
-          <Grid container spacing={32} justify="center" alignItems="center" direction="column" className={classes.main}>
-            <Grid item xs={6}>
-              <Typography variant="h2" color="primary" gutterBottom>
+          <Grid container spacing={32} justify="center" className={classes.main}>
+            <Grid item xs={12}>
+              <Typography variant="subtitle2" align="center" color="primary" className={classes.title}>
                 Services
               </Typography>
+              <Typography variant="h4" align="center">
+                Advantages
+              </Typography>
             </Grid>
+            <Grid item xs={12} md={8}>
+              <Typography variant="subtitle2" color="textSecondary" align="center" className={classes.description}>
+                We believe in building software that improves lives and makes us a little happier
+              </Typography>
+            </Grid> 
+            <Grid item xs={12}>
+              <Grid container spacing={32} justify="center" className={classes.papers}>
+                <Grid item xs={12} md={3}>
+                  <Paper className={classes.paper} elevation={1}>
+                    <FingerprintOutlined fontSize="large" color="primary" className={classes.icon} />
+                    <Typography variant="h6" align="center">
+                      Up to date technologies
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" align="center" className={classes.description}>
+                      As capabilities of technology are expected to continue growing exponentially, it is certain that the 
+                      future of business will increasingly efficient, streamlined, and will move at a faster pace.
+                    </Typography>
+                  </Paper>
+                </Grid> 
+                <Grid item xs={12} md={3}>
+                  <Paper className={classes.paper} elevation={1}>
+                    <HighQualityOutlined fontSize="large" color="primary" className={classes.icon} />
+                    <Typography variant="h6" align="center">
+                      High quality and balanced cost
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" align="center" className={classes.description}>
+                      It’s easier than you think to balance quality and cost for your business when real professionals come to work.
+                    </Typography>
+                  </Paper>
+                </Grid> 
+                <Grid item xs={12} md={3}>
+                  <Paper className={classes.paper} elevation={1}>
+                    <PeopleOutlined fontSize="large" color="primary" className={classes.icon} />
+                    <Typography variant="h6" align="center">
+                      Individual approach
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" align="center" className={classes.description}>
+                      Every person wants to feel special, and an individual approach lets us give you the care you need.
+                    </Typography>
+                  </Paper>
+                </Grid> 
+                <Grid item xs={12} md={3}>
+                  <Paper className={classes.paper} elevation={1}>
+                    <VerifiedUserOutlined fontSize="large" color="primary" className={classes.icon} />
+                    <Typography variant="h6" align="center">
+                      Warranty and support
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" align="center" className={classes.description}>
+                      When we talk about warranty, we mean much more, namely warmth and care that we give to our loved ones.
+                    </Typography>
+                  </Paper>
+                </Grid> 
+              </Grid>
+            </Grid> 
           </Grid>
         </SectionLayout>
       </section>
--- src/components/Services/Services.style.js
+++ src/components/Services/Services.style.js
@@ -1,5 +1,26 @@
 export default (theme) => ({
   services: {
     minHeight: 600,
-  }
+  },
+  main: {
+    paddingTop: `${theme.spacing.unit * 6}px`,
+    paddingBottom: `${theme.spacing.unit * 12}px`,
+  },
+  title: {
+    textTransform: 'uppercase',
+  },
+  description: {
+    margin: `0 ${theme.spacing.unit * 5}px`,
+  },
+  papers: {
+    marginTop: `${theme.spacing.unit * 2}px`,
+  },
+  paper: {
+    height: '100%',
+    padding: `${theme.spacing.unit * 2}px`,
+  },
+  icon: {
+    width: '100%',
+    margin: `${theme.spacing.unit * 2}px auto`,
+  },
 })
