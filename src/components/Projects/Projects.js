0f28cb4c2a0e5059ee61213a84069371270f6b53
Ivan Dovhal <limetrack@gmail.com>
2018-10-17 19:21:28 +0300

    Projects section updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import { Grid, Paper, Typography } from '@material-ui/core';
 import { SectionLayout } from 'containers';
 import styles from './Projects.style.js';
 
@@ -13,12 +12,42 @@ class Projects extends Component {
     return (
       <section className={classes.projects}>
         <SectionLayout>
-          <Grid container spacing={32} justify="center" alignItems="center" direction="column" className={classes.main}>
-            <Grid item xs={6}>
-              <Typography variant="h2" color="primary" gutterBottom>
-                Projects
+          <Grid container spacing={32} justify="center" className={classes.main}>
+            <Grid item xs={12}>
+              <Typography variant="h4" align="center">
+                Our works
               </Typography>
             </Grid>
+            <Grid item xs={12} md={8}>
+              <Typography variant="subtitle2" color="textSecondary" align="center">
+                We value our every client and look forward to knowing you
+              </Typography>
+            </Grid> 
+            <Grid item xs={12}>
+              <Grid container spacing={32} justify="center" className={classes.papers}>
+                <Grid item xs={12} md={3}>
+                  <Paper className={classes.paper} elevation={1}>
+                    <Typography variant="h6" align="center">
+                      Video Intelligence
+                    </Typography>
+                  </Paper>
+                </Grid> 
+                <Grid item xs={12} md={3}>
+                  <Paper className={classes.paper} elevation={1}>
+                    <Typography variant="h6" align="center">
+                      Newtend
+                    </Typography>
+                  </Paper>
+                </Grid> 
+                <Grid item xs={12} md={3}>
+                  <Paper className={classes.paper} elevation={1}>
+                    <Typography variant="h6" align="center">
+                      TeslaEnergo
+                    </Typography>
+                  </Paper>
+                </Grid>
+              </Grid>
+            </Grid> 
           </Grid>
         </SectionLayout>
       </section>
--- src/components/Projects/Projects.style.js
+++ src/components/Projects/Projects.style.js
@@ -2,4 +2,25 @@ export default (theme) => ({
   projects: {
     minHeight: 600,
   },
+  main: {
+    paddingTop: `${theme.spacing.unit * 6}px`,
+    paddingBottom: `${theme.spacing.unit * 12}px`,
+  },
+  title: {
+    textTransform: 'uppercase',
+  },
+  description: {
+    margin: `0 ${theme.spacing.unit * 2}px`,
+  },
+  papers: {
+    marginTop: `${theme.spacing.unit * 2}px`,
+  },
+  paper: {
+    height: '100%',
+    padding: `${theme.spacing.unit * 1}px`,
+  },
+  icon: {
+    width: '100%',
+    margin: `${theme.spacing.unit * 2}px auto`,
+  },
 })
