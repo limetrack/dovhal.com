84e6b83fc67d175ce55b9f1f54e5fecfe51b4cca
Ivan Dovhal <limetrack@gmail.com>
2018-10-22 10:27:50 +0300

    Consultation section updated

--- src/components/Consultation/Consultation.style.js
+++ src/components/Consultation/Consultation.style.js
@@ -1,6 +1,8 @@
 export default (theme) => ({
   consultation: {
     // minHeight: `${theme.spacing.unit * 40}px`,
+    paddingTop: `${theme.spacing.unit * 5}px`,
+    paddingBottom: `${theme.spacing.unit * 5}px`,
     backgroundColor: theme.palette.grey['100'],
     [theme.breakpoints.up('md')]: {
       backgroundImage: `linear-gradient(left, transparent 50%, ${theme.palette.primary.main} 50%, ${theme.palette.primary.main} 100%)`,
@@ -14,5 +16,8 @@ export default (theme) => ({
   },
   friends: {
     backgroundColor: theme.palette.primary.main,
+  },
+  textField: {
+    backgroundColor: theme.palette.grey['50'],
   },
 })
